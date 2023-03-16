bf745adc2d7b707f53fc9b45ef4bb79e2e216ca2
diegoram23 <diego.pta.oc@gmail.com>
2023-03-16 15:15:50 -0700

    displaying news related to ticker searched by user

--- src/pages/About.js
+++ src/pages/About.js
@@ -19,6 +19,13 @@ const About = ({ tickerName }) => {
     console.log('trimmed', newsArticles)
     return (
         <div>
+            {newsArticles.map(article =>
+                <div className='news-container' key={article.id}>
+                    <a href={article.url}><h4 className="news-headline">{article.headline}</h4></a>
+                    <img className='news-image' src={article.image} />
+                    <p className="news-time"> {new Date(article.datetime * 1000).toLocaleString("en-us")}</p>
+                    <p className="news-summary">{article.summary}</p>
+                </div>)}
             <p>{tickerName}</p>
         </div>
     );

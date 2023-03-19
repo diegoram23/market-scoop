9e0f30f84ae76c8513b3d7d4d65be2d69d02ceaa
diegoram23 <diego.pta.oc@gmail.com>
2023-03-19 15:03:01 -0700

    refactored useEffect fetch for home screen

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,31 +1,17 @@
-const Stats = ({ qqq, aapl, spy, dia }) => {
-
+const Stats = ({  all }) => {
     const styles = {
-        color: qqq.dp < 0 ? 'red' : 'green'
+        color: all.dp < 0 ? 'red' : 'green'
     }
-    
     return (
         <main className="stats-container">
-            <section className="stats-card" >
-                <h2 className="stats-name">QQQ</h2>
-                <p className="stats-percent" style={styles}>{Number(qqq.dp).toFixed(2)}%</p>
-                <p className="stats-price">{qqq.c}</p>
-            </section>
-            <section className="stats-card">
-                <h2 className="stats-name">AAPL</h2>
-                <p className="stats-percent">{Number(aapl.dp).toFixed(2)}%</p>
-                <p className="stats-price">{aapl.c}</p>
-            </section>
-            <section className="stats-card">
-                <h2 className="stats-name">SPY</h2>
-                <p className="stats-percent">{Number(spy.dp).toFixed(2)}%</p>
-                <p className="stats-price">{spy.c}</p>
-            </section>
-            <section className="stats-card">
-                <h2 className="stats-name">DIA</h2>
-                <p className="stats-percent">{Number(dia.dp).toFixed(2)}%</p>
-                <p className="stats-price">{dia.c}</p>
-            </section>
+            {all.map((all, i) =>
+                <section className="stats-card"  style={styles} key={i}>
+                    <h2 className="stats-name">{all.name}</h2>
+                    <p className="stats-percent" style={styles} >{all.dp.toFixed(2)}%</p>
+                    <p className="stats-price">{all.c}</p>
+                </section>
+            )}
+
         </main>
     )
 }
